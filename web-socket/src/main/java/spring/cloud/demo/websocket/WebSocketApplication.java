fd5ca1025ce3934283e578fc13a7c7149476546f
huangdaren <huangdaren1997@gmail.com>
2020-06-09 18:51:54 +0800

    init

---
+++ web-socket/src/main/java/spring/cloud/demo/websocket/WebSocketApplication.java
@@ -0,0 +1,47 @@
+package spring.cloud.demo.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import spring.cloud.demo.websocket.websocket.WebSocketServer;
+
+import java.io.IOException;
+
+@RestController
+@SpringBootApplication
+public class WebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+    @GetMapping("index")
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("请求成功");
+    }
+
+    @GetMapping("page")
+    public ModelAndView page() {
+        return new ModelAndView("websocket");
+    }
+
+    @RequestMapping("/push/{toUserId}")
+    public ResponseEntity<String> pushToWeb(String message, @PathVariable String toUserId) throws IOException {
+        WebSocketServer.sendInfo(message, toUserId);
+        return ResponseEntity.ok("MSG SEND SUCCESS");
+    }
+
+
+}
